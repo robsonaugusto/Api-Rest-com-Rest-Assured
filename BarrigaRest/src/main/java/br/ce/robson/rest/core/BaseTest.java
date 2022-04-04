083e4781f0a50f7704fb4cf898505ae2be729b91
robsonaugusto <robsonaugusto_15@hotmail.com>
2022-04-04 15:55:37 -0300

    Api Rest

---
+++ BarrigaRest/src/main/java/br/ce/robson/rest/core/BaseTest.java
@@ -0,0 +1,34 @@
+package br.ce.robson.rest.core;
+
+
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+public class BaseTest implements Constantes {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = APP_BASE_URL;
+		RestAssured.port = APP_PORT;
+		RestAssured.basePath = APP_BASE_PATH;
+		
+		
+		RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+		reqBuilder.setContentType(APP_CONTENT_TYPE);
+		RestAssured.requestSpecification = reqBuilder.build();
+		
+		ResponseSpecBuilder resbBuilder = new ResponseSpecBuilder();
+		resbBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+		RestAssured.responseSpecification = resbBuilder.build();
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		
+	}
+	
+	
+
+}
